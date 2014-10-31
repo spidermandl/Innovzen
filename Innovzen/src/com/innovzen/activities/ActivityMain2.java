e20ef8d60c9cfa733086375404c782d5b80d9e2d
chen <314830497@qq.com>
2014-10-31 17:51:35 +0800

    tiaozhuan  chy

---
+++ Innovzen/src/com/innovzen/activities/ActivityMain2.java
@@ -0,0 +1,17 @@
+package com.innovzen.activities;
+
+import com.innovzen.o2chair.R;
+
+import android.os.Bundle;
+
+public class ActivityMain2 extends ActivityBase{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_user_application_button);
+	}
+
+	
+}
---
+++ Innovzen/src/com/innovzen/fragments/FragMusic.java
@@ -0,0 +1,34 @@
+package com.innovzen.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+
+import com.innovzen.fragments.base.FragBase;
+import com.innovzen.o2chair.R;
+
+public class FragMusic extends FragBase implements OnClickListener{
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_music, container, false);
+
+        init(view);
+
+        return view;
+    }
+	@Override
+	public void onClick(View v) {
+		
+		
+	}
+
+	@Override
+	public void init(View view) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
