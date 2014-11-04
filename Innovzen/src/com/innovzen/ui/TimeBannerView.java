f26e1df73bd2f453c4ae9b8803141413c748de8e
Desmond <spiderman.dl@gmail.com>
2014-11-04 19:29:32 +0800

    reset

---
+++ Innovzen/src/com/innovzen/ui/TimeBannerView.java
@@ -0,0 +1,31 @@
+package com.innovzen.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class TimeBannerView extends ImageView {
+
+	public TimeBannerView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public TimeBannerView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public TimeBannerView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+	}
+}
